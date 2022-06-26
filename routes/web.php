454f496492a204725c23751eace4deb52ee8ae68
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-06-26 10:36:37 +0200

    Laravel 9 Breeze Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Demo\DemoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(DemoController::class)->group(function () {
-    Route::get('/about', 'Index')->middleware('check');
-    Route::get('/contact', 'ContactMethod');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
